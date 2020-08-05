01ad8d7420c2e259b1011d787ba6273de8089b5b
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-08-05 02:12:10 -0300

    Initializing routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express'
 
+const morgan = require('morgan')
+
+import { router } from './routes'
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('short'))
+app.use(router)
 
 export { app }
