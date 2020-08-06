df181878339f140637174374a01264e490b8e158
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-08-05 23:04:37 -0300

    Improving test's mock

--- test/product/mock.utils.ts
+++ test/product/mock.utils.ts
@@ -1,18 +1,23 @@
 import { Product } from '../../src/product/dto/product'
 
+import { v4 } from 'uuid'
+
 const generateProduct = (name: string) => {
-    return new Product({
+    const id: string = v4()
+    const obj: any = {
         name: name,
         description: '',
         price: 20.5,
         category: 'Some category',
         remainingUnits: 10,
         created: new Date(),
-    })
+    }
+
+    return new Product({ ...obj, id }, id)
 }
 
-const wrapper = async <T> (wrap: T) => {
+const wrapper = async <T>(wrap: T) => {
     return await wrap
 }
 
-export {generateProduct, wrapper} 
+export { generateProduct, wrapper }
